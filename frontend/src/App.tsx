964d0e17b3662f4c50572fde378f441c7fa9ee4d
asem259 <alghabra.asem@gmail.com>
2022-11-24 11:59:20 +0100

    feat: add needed dependencies.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import {MantineProvider} from "@mantine/core";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div >
-    </div>
-  )
+    return (
+        <MantineProvider theme={{colorScheme: 'light'}} withGlobalStyles withNormalizeCSS>
+            <Routes>
+                <Route path="/" element={<div>Dashboard</div>}/>
+            </Routes>
+        </MantineProvider>
+    )
 }
 
 export default App
