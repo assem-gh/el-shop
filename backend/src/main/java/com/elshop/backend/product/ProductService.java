a33303282d01aea6297261b1967c49926f37ff58
asem259 <alghabra.asem@gmail.com>
2022-11-25 00:29:32 +0100

    feat(api): add getById service & controller

--- backend/src/main/java/com/elshop/backend/product/ProductService.java
+++ backend/src/main/java/com/elshop/backend/product/ProductService.java
@@ -1,10 +1,14 @@
 package com.elshop.backend.product;
 
 import com.elshop.backend.common.AppUtils;
+import com.elshop.backend.exception.ResourceNotFoundException;
 import com.elshop.backend.product.model.Product;
 import com.elshop.backend.product.model.request.ProductRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +17,14 @@ public class ProductService {
     private final AppUtils utils;
     private final ProductRepository productRepository;
 
+    public Product getById(String id) {
+        Optional<Product> productToFind = productRepository.findById(id);
+
+        if (productToFind.isEmpty()) {
+            throw new ResourceNotFoundException("Product", id);
+        }
+        return productToFind.get();
+    }
 
     public Product createNewProduct(ProductRequest newProductData) {
         String id = utils.generateUuid();
