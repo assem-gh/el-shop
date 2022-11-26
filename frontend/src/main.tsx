33d44d03f6c7771d345d2ceaae000996ec02355d
asem259 <alghabra.asem@gmail.com>
2022-11-26 22:59:44 +0100

    add redux provider and product service.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
-          <App />
+        <App />
       </BrowserRouter>
-
+    </Provider>
+    ,
   </React.StrictMode>
-)
+);
