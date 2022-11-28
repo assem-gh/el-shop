933f345ee58de74b5ea2478e5be82993247eb8e2
asem259 <alghabra.asem@gmail.com>
2022-11-28 18:55:08 +0100

    feat: add dark theme.

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -1,40 +1,57 @@
-import React from 'react';
-import {ActionIcon, Avatar, Box, Group, TextInput} from "@mantine/core";
-import {TbSearch} from "react-icons/tb";
-import {FiBell} from "react-icons/fi";
+import React from "react";
+import {
+  ActionIcon,
+  Avatar,
+  Box,
+  Group,
+  TextInput,
+  useMantineColorScheme,
+} from "@mantine/core";
+import { TbMoonStars, TbSearch, TbSun } from "react-icons/tb";
+import { FiBell } from "react-icons/fi";
 
 import useStyles from "./header.style";
 
-
 const Header = () => {
-    const {classes} = useStyles();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
 
-    return (
-        <Box
-            component="header"
-            py="xs"
-            className={classes.header}
-        >
-            <Group spacing="lg" className={classes.inner} noWrap>
-                <Group>
-                    <TextInput
-                        rightSection={<ActionIcon>
-                            <TbSearch size={16}/>
-                        </ActionIcon>}
-                        radius="sm"
-                        placeholder="Search ..."/>
-                </Group>
+  const { classes } = useStyles();
 
-                <Group ml="auto">
-                    <Group ml={50} spacing={16}>
-                        <FiBell size={24}/>
-                        <Avatar color="cyan" radius="xl">AD</Avatar>
-                    </Group>
-                </Group>
+  return (
+    <Box component="header" py="xs" className={classes.header}>
+      <Group spacing="lg" className={classes.inner} noWrap>
+        <Group>
+          <TextInput
+            rightSection={
+              <ActionIcon>
+                <TbSearch size={16} />
+              </ActionIcon>
+            }
+            radius="sm"
+            placeholder="Search ..."
+          />
+        </Group>
 
-            </Group>
-        </Box>
-    );
+        <Group ml="auto">
+          <Group ml={50} spacing={16}>
+            <ActionIcon
+              variant="outline"
+              color={dark ? "yellow" : "blue"}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme"
+            >
+              {dark ? <TbSun size={22} /> : <TbMoonStars size={22} />}
+            </ActionIcon>
+            <FiBell size={24} />
+            <Avatar color="cyan" radius="xl">
+              AD
+            </Avatar>
+          </Group>
+        </Group>
+      </Group>
+    </Box>
+  );
 };
 
 export default Header;
--- frontend/src/components/layout/Header/header.style.ts
+++ frontend/src/components/layout/Header/header.style.ts
@@ -1,15 +1,16 @@
-import {createStyles} from "@mantine/core";
+import { createStyles } from "@mantine/core";
+import { getThemeColor } from "../../../utils";
 
 const useStyles = createStyles((theme) => ({
-header:{
+  header: {
     padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-    borderBottom: `1px solid ${theme.colors.gray[2]}`,
-    backgroundColor: theme.white,
+    borderBottom: `1px solid ${getThemeColor(theme, 4, 2)}`,
+    backgroundColor: getThemeColor(theme, 7),
     height: 72,
-},
-    inner:{
-    height:"100%"
-    },
-}))
+  },
+  inner: {
+    height: "100%",
+  },
+}));
 
 export default useStyles;
