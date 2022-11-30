8752236eb624e41c8c47978ab137411c5060487e
asem259 <alghabra.asem@gmail.com>
2022-11-30 17:08:39 +0100

    add cors config and remove S3 mock dependency.

--- backend/src/main/java/com/elshop/backend/product/ProductController.java
+++ backend/src/main/java/com/elshop/backend/product/ProductController.java
@@ -20,7 +20,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-@CrossOrigin
 public class ProductController {
 
     private final ProductService productService;
