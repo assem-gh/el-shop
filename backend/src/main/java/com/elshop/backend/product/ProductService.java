218ae1d2157fb5c6ddd7956f1fb37f754cc3d05f
asem259 <alghabra.asem@gmail.com>
2022-12-04 19:19:11 +0100

    refactor.

--- backend/src/main/java/com/elshop/backend/product/ProductService.java
+++ backend/src/main/java/com/elshop/backend/product/ProductService.java
@@ -1,6 +1,6 @@
 package com.elshop.backend.product;
 
-import com.elshop.backend.common.AppUtils;
+import com.elshop.backend.common.KeyGenerateService;
 import com.elshop.backend.exception.ResourceNotFoundException;
 import com.elshop.backend.product.model.Product;
 import com.elshop.backend.product.model.request.ProductRequest;
@@ -18,7 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ProductService {
 
-    private final AppUtils utils;
+    private final KeyGenerateService keyGenerateService;
     private final ProductRepository productRepository;
 
     public Product getById(String id) {
@@ -31,7 +31,7 @@ public class ProductService {
     }
 
     public Product createNewProduct(ProductRequest newProductData, String id, List<String> images) {
-        String slug = utils.generateSlug(newProductData.title());
+        String slug = keyGenerateService.generateSlug(newProductData.title());
 
         return productRepository.save(new Product(id, slug,
                 newProductData.title(), newProductData.price(),
