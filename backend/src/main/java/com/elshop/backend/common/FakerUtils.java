833d2a55f18e4add228817c111c85caf1013abf1
asem259 <alghabra.asem@gmail.com>
2022-12-04 19:19:11 +0100

    refactor.

--- backend/src/main/java/com/elshop/backend/common/FakerUtils.java
+++ backend/src/main/java/com/elshop/backend/common/FakerUtils.java
@@ -3,41 +3,38 @@ package com.elshop.backend.common;
 import com.elshop.backend.product.model.Product;
 import com.github.javafaker.Faker;
 
-import java.util.Arrays;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class FakerUtils {
 
     private static final Faker faker = new Faker();
 
-    private static final AppUtils appUtils = new AppUtils();
+    private static final KeyGenerateService RANDOM_IDS_SERVICE = new KeyGenerateService();
 
     private FakerUtils() {
     }
 
-    public static Product generateFakeProduct(int imagesNr) {
-        List<String> images = generateImagesList(imagesNr);
-        String id = appUtils.generateUuid();
+    public static Product generateFakeProduct(int numberOfImages) {
+        List<String> images = generateImagesList(numberOfImages);
+        String id = RANDOM_IDS_SERVICE.generateUuid();
         String title = faker.commerce().productName();
-        String slug = appUtils.generateSlug(title);
+        String slug = RANDOM_IDS_SERVICE.generateSlug(title);
 
-        double price = Double.parseDouble(faker.commerce().price(1, 1000));
+        BigDecimal price = new BigDecimal(faker.commerce().price(1, 1000));
         return new Product(id, slug, title, price, images, "Mobile");
     }
 
-    public static List<Product> generateFakeProductsList(int productsNr) {
-
-        List<Product> list = Arrays.asList(new Product[productsNr]);
-
-        return list.stream()
-                .map(item -> generateFakeProduct(faker.number().numberBetween(1, 5)))
+    public static List<Product> generateFakeProductsList(int numberOfProducts) {
+        return IntStream.range(0, numberOfProducts)
+                .mapToObj(x -> generateFakeProduct(faker.number().numberBetween(1, 5)))
                 .toList();
     }
 
-    private static List<String> generateImagesList(int imagesNr) {
-        List<String> list = Arrays.asList(new String[imagesNr]);
-        return list.stream()
-                .map(item -> faker.internet().image())
+    private static List<String> generateImagesList(int numberOfImages) {
+        return IntStream.range(0, numberOfImages)
+                .mapToObj(item -> faker.internet().image())
                 .toList();
     }
 }
