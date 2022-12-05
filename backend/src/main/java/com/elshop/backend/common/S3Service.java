87dd41f5c3c6157f61cd8e041baa15ae727d0ce3
asem259 <alghabra.asem@gmail.com>
2022-12-05 04:28:09 +0100

    refactor

--- backend/src/main/java/com/elshop/backend/common/S3Service.java
+++ backend/src/main/java/com/elshop/backend/common/S3Service.java
@@ -1,21 +1,25 @@
 package com.elshop.backend.common;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.elshop.backend.exception.UploadFileException;
+import com.elshop.backend.exception.UploadFileMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class S3Service {
@@ -31,52 +35,38 @@ public class S3Service {
     public List<String> uploadMultipleFiles(MultipartFile[] images, String type, String entityId) {
         return Arrays.stream(images)
                 .map(image -> uploadFile(image, type, entityId))
-                .toList();
+                .collect(Collectors.toList());
+
     }
+
 
     public String uploadFile(MultipartFile file, String type, String entityId) {
-        String originalFilename = file.getOriginalFilename();
-        String bucketKey;
-        try {
-            File imageFile = convertMultiPartToFile(file);
-            bucketKey = String.format("%s/%s/%s", type, entityId, originalFilename);
-            s3.putObject(bucketName, bucketKey, imageFile);
-            Files.delete(imageFile.toPath());
-            return s3.getUrl(bucketName, bucketKey).toString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+        if (!file.getContentType().startsWith("image/")) {
+            throw new UploadFileException(HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                    UploadFileMessage.INVALID_IMAGE_TYPE, null);
         }
-
-    }
-
-    public void deleteFileByName(final String keyName) {
-        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucketName, keyName);
-        s3.deleteObject(deleteObjectRequest);
-    }
-
-    public void deleteAll(final String prefix, String bucketName) {
-        ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
-                .withBucketName(bucketName)
-                .withPrefix(prefix);
-        ObjectListing objectListing = s3.listObjects(listObjectsRequest);
-
-        while (true) {
-            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
-                s3.deleteObject(bucketName, objectSummary.getKey());
-            }
-            if (objectListing.isTruncated()) {
-                objectListing = s3.listNextBatchOfObjects(objectListing);
-            } else {
-                break;
-            }
+        Path bucketKey;
+        try (InputStream inputStream = file.getInputStream()) {
+            bucketKey = Paths.get(type, entityId, UUID.randomUUID().toString());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+            TransferManager transferManager = TransferManagerBuilder.defaultTransferManager();
+            PutObjectRequest request = new PutObjectRequest(bucketName, bucketKey.toString(), inputStream, objectMetadata);
+            Upload upload = transferManager.upload(request);
+            upload.waitForCompletion();
+            return s3.getUrl(bucketName, bucketKey.toString()).toString();
+        } catch (Exception e) {
+            String errorMessage = String.format(
+                    "Error uploading file '%s' with size %d and content type '%s':%n%s",
+                    file.getOriginalFilename(),
+                    file.getSize(),
+                    file.getContentType(),
+                    e.getMessage() != null ? e.getMessage() : "Unknown error"
+            );
+            throw new UploadFileException(HttpStatus.SERVICE_UNAVAILABLE, UploadFileMessage.UPLOAD_ERROR, errorMessage);
         }
     }
 
-    private File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
-        File file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(multipartFile.getBytes());
-        }
-        return file;
-    }
 }
