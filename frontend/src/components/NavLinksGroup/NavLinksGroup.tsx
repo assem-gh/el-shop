d2f7c14d092255ea4fcd84e4d25dff19cb613dfa
asem259 <alghabra.asem@gmail.com>
2022-12-05 04:38:06 +0100

    update nav items.

--- frontend/src/components/NavLinksGroup/NavLinksGroup.tsx
+++ frontend/src/components/NavLinksGroup/NavLinksGroup.tsx
@@ -1,100 +1,84 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 import { Collapse, Stack, Text, Tooltip, UnstyledButton } from "@mantine/core";
 import { IconType } from "react-icons";
 import useStyles from "./navGroup.style";
-import { BsDashSquare } from "react-icons/bs";
-import { Link as RouterLink } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { TbChevronDown } from "react-icons/tb";
+import NavLinkItem from "./NavLinkItem";
+import { Link } from "../../data/navLinks";
 
 interface NavLinksControlProps {
-  setActiveGroup: Dispatch<SetStateAction<string>>;
-  setCollapsed: Dispatch<SetStateAction<boolean>>;
+  setOpened: Dispatch<SetStateAction<boolean>>;
   name: string;
   collapsed: boolean;
   icon: IconType;
-  links: any;
-  isActive: boolean;
+  links: Link[];
+  path: string;
 }
 
 const NavLinksGroup = ({
-  setActiveGroup,
-  isActive,
   name,
   icon: Icon,
   links,
+  path,
   collapsed,
-  setCollapsed,
+  setOpened,
 }: NavLinksControlProps) => {
-  const { classes, cx, theme } = useStyles({ collapsed, isActive });
+  const [showLinks, setShowLinks] = useState(false);
 
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const isHasLinks = links.length > 0;
+
+  const isActive = useMemo(() => {
+    return pathname === path || links.some((l) => l.path === pathname);
+  }, [pathname]);
+
+  const { classes, cx, theme } = useStyles({ collapsed, isActive });
   const handleClick = () => {
-    if (collapsed) {
-      setCollapsed(false);
-      setActiveGroup(() => name);
-    } else {
-      setActiveGroup((prev) => (prev === name ? "" : name));
-    }
+    if (!isHasLinks) navigate(path);
+    else setShowLinks((prev) => !prev);
   };
 
+  const iconColor = isActive ? theme.colors[theme.primaryColor][6] : "inherit";
   return (
     <>
-      <Tooltip
-        disabled={!collapsed || links.length > 0}
-        label={name}
-        position="right"
-      >
-        <UnstyledButton
-          px="lg"
-          pb={0}
-          className={cx(classes.itemControl, classes.flex)}
-          onClick={handleClick}
+      {((collapsed && !isHasLinks) || !collapsed) && (
+        <Tooltip
+          disabled={!collapsed || isHasLinks}
+          label={name}
+          position="right"
         >
-          <Icon size={18} className={classes.leftIcon} />
-          <Text className={classes.label}>{name}</Text>
-          {links.length > 0 && (
-            <TbChevronDown size={18} className={classes.rightIcon} />
-          )}
-        </UnstyledButton>
-      </Tooltip>
-      {links.length > 0 && (
+          <UnstyledButton
+            px="lg"
+            pb={0}
+            className={cx(classes.itemControl, classes.flex)}
+            onClick={handleClick}
+          >
+            <Icon color={iconColor} size={18} className={classes.leftIcon} />
+            <Text className={classes.label}>{name}</Text>
+            {links.length > 0 && (
+              <TbChevronDown size={18} className={classes.rightIcon} />
+            )}
+          </UnstyledButton>
+        </Tooltip>
+      )}
+
+      {isHasLinks && (
         <Collapse
           py={collapsed ? 0 : "xs"}
-          bg={theme.fn.rgba(theme.colors[theme.primaryColor][6], 0.1)}
-          in={collapsed || isActive}
+          bg={
+            !collapsed
+              ? theme.fn.rgba(theme.colors[theme.primaryColor][6], 0.1)
+              : "inherit"
+          }
+          in={collapsed || showLinks}
         >
           <Stack spacing={4}>
-            {links.map(
-              (link: { label: string; path: string; icon: IconType }) => {
-                const LinkIcon = link.icon;
-
-                return (
-                  <Tooltip
-                    key={link.label}
-                    disabled={!collapsed}
-                    label={link.label}
-                    position="right"
-                  >
-                    <UnstyledButton
-                      className={cx(classes.flex, classes.navItem)}
-                      component={RouterLink}
-                      to={link.path}
-                      py={collapsed ? "md" : "xs"}
-                    >
-                      {collapsed ? (
-                        <LinkIcon size={14} />
-                      ) : (
-                        <>
-                          <BsDashSquare size={14} />
-                          <Text ml="md" sx={{ flexGrow: 1 }} size="sm">
-                            {link.label}
-                          </Text>
-                        </>
-                      )}
-                    </UnstyledButton>
-                  </Tooltip>
-                );
-              }
-            )}
+            {links.map((link) => (
+              <NavLinkItem key={link.path} collapsed={collapsed} {...link} />
+            ))}
           </Stack>
         </Collapse>
       )}
