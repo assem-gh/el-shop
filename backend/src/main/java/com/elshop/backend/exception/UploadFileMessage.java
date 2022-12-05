f7a45f83b04394a1ca610da44598e363ea8257ff
asem259 <alghabra.asem@gmail.com>
2022-12-05 05:13:10 +0100

    catch InterruptedException and IOException.

--- backend/src/main/java/com/elshop/backend/exception/UploadFileMessage.java
+++ backend/src/main/java/com/elshop/backend/exception/UploadFileMessage.java
@@ -4,7 +4,9 @@ public enum UploadFileMessage {
 
     NO_IMAGES_PROVIDED("No images were provided in the request."),
     INVALID_IMAGE_TYPE("The provided files are not valid images."),
-    UPLOAD_ERROR("An error occurred while uploading the files. Please try again later.");
+    UPLOAD_ERROR("An error occurred while uploading the files. Please try again later."),
+    FILE_READ_ERROR("An error occurred while reading the files.");
+
 
     private final String message;
 
