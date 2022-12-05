dfb6ddf55bfbe9dd5142f5e04cd37fbab4054bdd
asem259 <alghabra.asem@gmail.com>
2022-12-05 17:11:17 +0100

    update product service.

--- backend/src/main/java/com/elshop/backend/product/ProductService.java
+++ backend/src/main/java/com/elshop/backend/product/ProductService.java
@@ -23,11 +23,8 @@ public class ProductService {
 
     public Product getById(String id) {
         Optional<Product> productToFind = productRepository.findById(id);
-
-        if (productToFind.isEmpty()) {
-            throw new ResourceNotFoundException("Product", id);
-        }
-        return productToFind.get();
+        return productToFind
+                .orElseThrow(() -> new ResourceNotFoundException("Product", id));
     }
 
     public Product createNewProduct(ProductRequest newProductData, String id, List<String> images) {
@@ -35,7 +32,7 @@ public class ProductService {
 
         return productRepository.save(new Product(id, slug,
                 newProductData.title(), newProductData.price(),
-                images, newProductData.category()));
+                images, newProductData.category(), newProductData.brand(), newProductData.description()));
     }
 
     public ProductsListResponse getAll(int page, int size) {
@@ -52,10 +49,8 @@ public class ProductService {
 
 
     public void deleteProduct(String id) {
-        Optional<Product> optionalProduct = productRepository.findById(id);
-        if (optionalProduct.isEmpty()) {
-            throw new ResourceNotFoundException("Product", id);
-        }
-        productRepository.delete(optionalProduct.get());
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", id));
+        productRepository.delete(product);
     }
 }
