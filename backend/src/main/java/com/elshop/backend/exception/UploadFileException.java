155dfe5a3f23ddd4432e618701125c7b54140813
asem259 <alghabra.asem@gmail.com>
2022-12-05 04:10:02 +0100

    add upload files Exceptions.

--- backend/src/main/java/com/elshop/backend/exception/UploadFileException.java
+++ backend/src/main/java/com/elshop/backend/exception/UploadFileException.java
@@ -6,11 +6,17 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public class UploadFileException extends RuntimeException {
-    public static final String TYPE = HttpStatus.SERVICE_UNAVAILABLE.getReasonPhrase();
-    public static final String MESSAGE = "The server is currently unavailable, try again later!";
+    private final String errorType;
+    private final String errorMessage;
+
+    private final HttpStatus errorCode;
     private final String errorDetails;
 
-    public UploadFileException(String details) {
+    public UploadFileException(HttpStatus errorCode, UploadFileMessage message, String details) {
+        this.errorType = errorCode.getReasonPhrase();
+        this.errorCode = errorCode;
+        this.errorMessage = message.getMessage();
         this.errorDetails = details;
+
     }
 }
