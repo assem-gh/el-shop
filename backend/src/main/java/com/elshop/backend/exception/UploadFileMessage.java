b251abc4c5c9e7c7a467bf2bec767bf2fd9a7655
asem259 <alghabra.asem@gmail.com>
2022-12-05 04:10:02 +0100

    add upload files Exceptions.

---
+++ backend/src/main/java/com/elshop/backend/exception/UploadFileMessage.java
@@ -0,0 +1,24 @@
+package com.elshop.backend.exception;
+
+public enum UploadFileMessage {
+
+    NO_IMAGES_PROVIDED("No images were provided in the request."),
+    INVALID_IMAGE_TYPE("The provided files are not valid images."),
+    UPLOAD_ERROR("An error occurred while uploading the files. Please try again later.");
+
+    private final String message;
+
+    UploadFileMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
+
+
+
+
+
