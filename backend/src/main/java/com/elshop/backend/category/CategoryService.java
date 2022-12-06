22c51bfbc1e4254e6f4cef6afc4d6c3096f32cc2
asem259 <alghabra.asem@gmail.com>
2022-12-06 17:20:37 +0100

    Add GET support for retrieving one or all categories.

--- backend/src/main/java/com/elshop/backend/category/CategoryService.java
+++ backend/src/main/java/com/elshop/backend/category/CategoryService.java
@@ -4,9 +4,11 @@ import com.elshop.backend.category.model.Category;
 import com.elshop.backend.category.model.request.CategoryRequest;
 import com.elshop.backend.common.KeyGenerateService;
 import com.elshop.backend.exception.ResourceAlreadyExistException;
+import com.elshop.backend.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,5 +27,15 @@ public class CategoryService {
         }
         String id = keyGenerateService.generateUuid();
         return categoryRepository.save(new Category(id, requestData.name()));
+    }
+
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getById(String id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", id));
     }
 }
