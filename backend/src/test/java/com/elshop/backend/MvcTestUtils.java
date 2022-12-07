8bf0e616810a3f08c46630cd7ea343cd6f203a98
asem259 <alghabra.asem@gmail.com>
2022-12-07 16:10:37 +0100

    Add integration test update $delete category.

--- backend/src/test/java/com/elshop/backend/MvcTestUtils.java
+++ backend/src/test/java/com/elshop/backend/MvcTestUtils.java
@@ -1,5 +1,8 @@
 package com.elshop.backend;
 
+import com.elshop.backend.category.model.Category;
+import com.elshop.backend.category.model.request.CategoryRequest;
+import com.elshop.backend.exception.ErrorResponse;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +13,56 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RequiredArgsConstructor
 public class MvcTestUtils {
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String categoriesEndpoint = "/api/categories/";
+
 
     private final MockMvc mvc;
 
+    public Category performCategoryPostOperationExpectOk(CategoryRequest requestData) throws Exception {
+        return performMvcResourceOperation(requestData,
+                HttpMethod.POST,
+                categoriesEndpoint,
+                HttpStatus.OK,
+                new TypeReference<Category>() {
+                });
+    }
+
+    public ErrorResponse performCategoryPostOperationExpectError(CategoryRequest requestData, HttpStatus expectedStatus) throws Exception {
+        return performMvcResourceOperation(requestData,
+                HttpMethod.POST,
+                categoriesEndpoint,
+                expectedStatus,
+                new TypeReference<ErrorResponse>() {
+                });
+    }
+
+    public List<Category> performCategoryGetAllOperationExpectOk() throws Exception {
+        return performMvcResourceOperation(null,
+                HttpMethod.GET,
+                categoriesEndpoint,
+                HttpStatus.OK,
+                new TypeReference<List<Category>>() {
+                });
+    }
+
+    public <T> T performResourceGetOperation(HttpStatus expectedStatus, String uri, TypeReference<T> responseType) throws Exception {
+        return performMvcResourceOperation(null,
+                HttpMethod.GET,
+                uri,
+                expectedStatus,
+                responseType);
+    }
+
+    public <T> T performCategoriesPutOperation(CategoryRequest requestData, HttpStatus expectedStatus, String uri, TypeReference<T> responseType) throws Exception {
+        return performMvcResourceOperation(requestData, HttpMethod.PUT, uri, expectedStatus, responseType);
+    }
 
     public <T, R> T performMvcResourceOperation(R requestData, HttpMethod method,
                                                 String uri, HttpStatus expectedStatus,
@@ -33,12 +78,6 @@ public class MvcTestUtils {
                 .andExpect(status().is(expectedStatus.value()))
                 .andReturn().getResponse().getContentAsString();
         return objectMapper.readValue(res, responseType);
-    }
-
-    public <T> T performMvcResourceOperation(HttpMethod method,
-                                             String uri, HttpStatus expectedStatus,
-                                             TypeReference<T> responseType) throws Exception {
-        return performMvcResourceOperation(null, method, uri, expectedStatus, responseType);
     }
 
     public ObjectMapper mapper() {
