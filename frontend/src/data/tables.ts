83824313e769f6fb9aee321ff8158ae783d5bead
asem259 <alghabra.asem@gmail.com>
2022-12-08 22:35:35 +0100

    Add table data and update TableRow & TableHeader.

--- frontend/src/data/tables.ts
+++ frontend/src/data/tables.ts
@@ -1,5 +1,20 @@
-export const columns = {
-  products: ["Product", "Title", "Price", "Category"],
-  orders: [],
-  users: [],
+import { CategoryModel, ProductModel } from "../store/slices/model";
+
+type RowDataType<T> = Record<string, keyof T>;
+
+interface TableData {
+  product: RowDataType<ProductModel>;
+  category: RowDataType<CategoryModel>;
+}
+
+export const tableData: TableData = {
+  product: {
+    Product: "images",
+    Title: "title",
+    Category: "category",
+    Price: "price",
+  },
+  category: {
+    Name: "name",
+  },
 };
