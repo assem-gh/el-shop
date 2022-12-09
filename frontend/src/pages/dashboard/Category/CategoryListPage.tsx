bdaecfcab3a48890d2b0e2951dacde7747a76af2
asem259 <alghabra.asem@gmail.com>
2022-12-09 16:16:54 +0100

    fix minor issues.

--- frontend/src/pages/dashboard/Category/CategoryListPage.tsx
+++ frontend/src/pages/dashboard/Category/CategoryListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import PageHeader from "../../../components/PageHeader/PageHeader";
 import { Button } from "@mantine/core";
 import { MdAdd } from "react-icons/md";
@@ -7,16 +7,16 @@ import AddCategoryModal from "../../../components/Modal/AddCategoryModal";
 
 const CategoryListPage = () => {
   const [openModal, setOpenModal] = useState(false);
+  const handleAdd = useCallback(
+    () => setOpenModal((prev) => !prev),
+    [setOpenModal]
+  );
   return (
     <>
       <PageHeader
         title="Categories"
         actions={
-          <Button
-            onClick={() => setOpenModal((prev) => !prev)}
-            variant="light"
-            leftIcon={<MdAdd />}
-          >
+          <Button onClick={handleAdd} variant="light" leftIcon={<MdAdd />}>
             Add category
           </Button>
         }
