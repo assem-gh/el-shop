7bb41c8b7a58417bbba633c6a8bb168cca796693
asem259 <alghabra.asem@gmail.com>
2022-12-16 14:06:38 +0100

    add dashboard home page

---
+++ frontend/src/components/Stats/stat.style.tsx
@@ -0,0 +1,31 @@
+import { createStyles } from "@mantine/core";
+
+export const useStyles = createStyles((theme) => ({
+  root: {
+    padding: theme.spacing.xl * 1.5,
+  },
+
+  value: {
+    fontSize: 24,
+    fontWeight: 700,
+    lineHeight: 1,
+  },
+
+  diff: {
+    lineHeight: 1,
+    display: "flex",
+    alignItems: "center",
+  },
+
+  icon: {
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[3]
+        : theme.colors.gray[4],
+  },
+
+  title: {
+    fontWeight: 700,
+    textTransform: "uppercase",
+  },
+}));
